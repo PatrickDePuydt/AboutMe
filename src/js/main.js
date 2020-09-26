7f7a80a6329add216174462c1c50f247493bc5e9
Patrick DePuydt <prdepuydt@gmail.com>
2020-09-26 16:39:31 -0700

    Commit logic before I break everything

--- src/js/main.js
+++ src/js/main.js
@@ -2,45 +2,50 @@ $(window).scroll(function () {
   const scrollPosition = $(this).scrollTop(); // Grabbing the scroll position
   const backgroundImage = $("[data-target='background']");
 
-  const second_portait =
-    "https://images.unsplash.com/photo-1559017895-ca553c995892?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80";
+  const original_portait =
+    "url(https://images.unsplash.com/photo-1509460913899-515f1df34fea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80)";
 
-  const third_portrait =
-    "https://images.unsplash.com/photo-1598628340414-bbf4c9e21604?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
+  const portrait_two =
+    "url(https://images.unsplash.com/photo-1600496461900-3f1b74fb40d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=930&q=80)";
+
+  const portrait_three =
+    "url(https://images.unsplash.com/photo-1598628340414-bbf4c9e21604?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80)";
 
   //   Spacing Conditions
-  const scroll_one = scrollPosition > 100 && scrollPosition < 199;
-  const scroll_two = scrollPosition > 200 && scrollPosition < 299;
-  const scroll_three = scrollPosition > 300 && scrollPosition < 399;
-  const scroll_four = scrollPosition > 400 && scrollPosition < 499;
-  const scroll_five = scrollPosition > 500 && scrollPosition < 599;
-  const scroll_six = scrollPosition > 600 && scrollPosition < 699;
-  const scroll_seven = scrollPosition > 700 && scrollPosition < 799;
-  const scroll_eight = scrollPosition > 800 && scrollPosition < 899;
-  const scroll_nine = scrollPosition > 900 && scrollPosition < 999;
+  const one = scrollPosition > 100 && scrollPosition < 199;
+  const two = scrollPosition > 200 && scrollPosition < 299;
+  const three = scrollPosition > 300 && scrollPosition < 399;
+  const four = scrollPosition > 400 && scrollPosition < 499;
+  const five = scrollPosition > 500 && scrollPosition < 599;
+  const six = scrollPosition > 600 && scrollPosition < 699;
+  const seven = scrollPosition > 700 && scrollPosition < 799;
+  const eight = scrollPosition > 800 && scrollPosition < 899;
+  const nine = scrollPosition > 900 && scrollPosition < 999;
 
   //    Cycle the background image depending on where the user is on the page
-  if (scroll_one) {
-    console.log("One");
-  } else if (scroll_two) {
-    console.log("Two");
-    backgroundImage.css("background-image", second_portait);
-  } else if (scroll_three) {
-    console.log("Three");
-    backgroundImage.css("background-image", third_portrait);
-  } else if (scroll_four) {
-    console.log("scroll_four");
-  } else if (scroll_five) {
-    console.log("five");
-  } else if (scroll_six) {
-    console.log("scroll_six");
-  } else if (scroll_seven) {
-    console.log("scroll_seven");
-  } else if (scroll_eight) {
-    console.log("scroll_eight");
-  } else if (scroll_nine) {
-    console.log("scroll_nine");
+  if (one) {
+    // console.log("One");
+  } else if (two) {
+    // console.log("Two");
+    backgroundImage.css("background-image", portrait_two);
+  } else if (three) {
+    // console.log("Three");
+    backgroundImage.css("background-image", portrait_three);
+  } else if (four) {
+    // console.log("four");
+  } else if (five) {
+    // console.log("five");
+  } else if (six) {
+    // console.log("six");
+  } else if (seven) {
+    // console.log("seven");
+  } else if (eight) {
+    // console.log("eight");
+  } else if (nine) {
+    // console.log("nine");
   } else {
-    console.log("Default");
+    backgroundImage.css("background-image", original_portait);
   }
+
+  console.log(document.documentElement.scrollHeight / scrollPosition);
 });
