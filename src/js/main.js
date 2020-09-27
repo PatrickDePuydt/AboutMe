2135240e3c370514df67a4e23bb6ff35ff735ba3
Patrick DePuydt <prdepuydt@gmail.com>
2020-09-26 19:56:47 -0700

    Clean up comments, markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,30 @@
 window.addEventListener("scroll", function () {
+  // When a user starts scrolling...
   const scrollPosition = Math.round(this.scrollY); // Grab the scroll position, rounding these suckers off because aint nobody got time for that
   const documentHeight = document.documentElement.scrollHeight; // Gonna need this for some fancy pants math
   const userViewingAt = (scrollPosition / documentHeight) * 100; // Where the user is currently scrolling
-  const backgroundImage = document.querySelector("[data-target='portrait']"); // The body element for the background
+  const backgroundELement = document.querySelector("[data-target='portrait']"); // The body element for the background
 
-  if (userViewingAt < 9) {
-    backgroundImage.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundImage.classList.add("portrait_one"); // Set the portrait back to the first
-  } else if (userViewingAt >= 10 && userViewingAt <= 20) {
-    backgroundImage.classList.remove("portrait_one"); // Remove the old
-    backgroundImage.classList.add("portrait_two"); // Add the newf
+  if (userViewingAt < 20) {
+    backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
+    backgroundELement.classList.add("portrait_one"); // Set the portrait back to the first
   } else if (userViewingAt >= 20 && userViewingAt <= 40) {
-    backgroundImage.classList.remove("portrait_two"); // Remove the old
-    backgroundImage.classList.add("portrait_three"); // Add the new
+    backgroundELement.classList.remove("portrait_one"); // Remove the old
+    backgroundELement.classList.add("portrait_two"); // Add the new
   } else if (userViewingAt >= 40 && userViewingAt <= 60) {
-    backgroundImage.classList.remove("portrait_three"); // Remove the old
-    backgroundImage.classList.add("portrait_four"); // Add the new
-  } else if (userViewingAt >= 60 && userViewingAt <= 90) {
-    backgroundImage.classList.remove("portrait_four"); // Remove the old
-    backgroundImage.classList.add("portrait_five"); // Add the new
+    backgroundELement.classList.remove("portrait_two"); // Remove the old
+    backgroundELement.classList.add("portrait_three"); // Add the new
+  } else if (userViewingAt >= 60 && userViewingAt <= 80) {
+    backgroundELement.classList.remove("portrait_three"); // Remove the old
+    backgroundELement.classList.add("portrait_four"); // Add the new
+  } else if (userViewingAt >= 80 && userViewingAt <= 100) {
+    backgroundELement.classList.remove("portrait_four"); // Remove the old
+    backgroundELement.classList.add("portrait_five"); // Add the new
   } else {
-    backgroundImage.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundImage.classList.add("portrait_one"); // Set the portrait back to the first
+    backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
+    backgroundELement.classList.add("portrait_one"); // Set the portrait back to the first
   }
 
   //   console.log("userViewingAt: ", userViewingAt);
-  //   console.log("backgroundImage: ", backgroundImage);
+  //   console.log("backgroundELement: ", backgroundELement);
 });
