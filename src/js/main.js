586a1553dc061a1f7f1e9be50cbcc5ed3df51e66
Patrick DePuydt <prdepuydt@gmail.com>
2020-09-27 02:56:45 -0700

    Reassign portraits

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,35 @@ window.addEventListener("scroll", function () {
   const userViewingAt = (scrollPosition / documentHeight) * 100; // Where the user is currently scrolling
   const backgroundELement = document.querySelector("[data-target='portrait']"); // The body element for the background
 
-  if (userViewingAt < 20) {
+  if (userViewingAt < 10) {
     backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundELement.classList.add("portrait_one"); // Set the portrait back to the first
-  } else if (userViewingAt >= 20 && userViewingAt <= 40) {
+    backgroundELement.classList.add("portrait_1"); // Set the portrait back to the first
+  } else if (userViewingAt >= 10 && userViewingAt <= 20) {
     backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundELement.classList.add("portrait_two"); // Add the new
-  } else if (userViewingAt >= 40 && userViewingAt <= 60) {
+    backgroundELement.classList.add("portrait_2"); // Add the new
+  } else if (userViewingAt >= 20 && userViewingAt <= 30) {
     backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundELement.classList.add("portrait_three"); // Add the new
-  } else if (userViewingAt >= 60 && userViewingAt <= 80) {
+    backgroundELement.classList.add("portrait_3"); // Add the new
+  } else if (userViewingAt >= 30 && userViewingAt <= 40) {
     backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundELement.classList.add("portrait_four"); // Add the new
-  } else if (userViewingAt >= 80 && userViewingAt <= 100) {
+    backgroundELement.classList.add("portrait_4"); // Add the new
+  } else if (userViewingAt >= 40 && userViewingAt <= 50) {
     backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundELement.classList.add("portrait_five"); // Add the new
+    backgroundELement.classList.add("portrait_5"); // Add the new
+  } else if (userViewingAt >= 50 && userViewingAt <= 60) {
+    backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
+    backgroundELement.classList.add("portrait_6"); // Add the new
+  } else if (userViewingAt >= 60 && userViewingAt <= 70) {
+    backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
+    backgroundELement.classList.add("portrait_7"); // Add the new
+  } else if (userViewingAt >= 70 && userViewingAt <= 80) {
+    backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
+    backgroundELement.classList.add("portrait_8"); // Add the new
+  } else if (userViewingAt >= 80 && userViewingAt <= 90) {
+    backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
+    backgroundELement.classList.add("portrait_9"); // Add the new
   } else {
     backgroundELement.setAttribute("class", " "); // Blow out the classes, start over
-    backgroundELement.classList.add("portrait_one"); // Set the portrait back to the first
+    backgroundELement.classList.add("portrait_1"); // Set the portrait back to the first
   }
-
-  //   console.log("userViewingAt: ", userViewingAt);
-  //   console.log("backgroundELement: ", backgroundELement);
 });
